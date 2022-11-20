f308a334246fb9a3b9d95dedce984467b177620b
Jizong's Fox <jizong.peng.1@etsmtl.net>
2022-11-20 16:07:40 -0500

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -55,7 +55,7 @@ custom_zsh=${ZSH:+yes}
 
 # Default settings
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
-REPO=${REPO:-jizong/ohmyzsh}
+REPO=${REPO:-jizongfox/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
